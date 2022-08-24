26ef98d30eea0af01e641610355b05fbd3fb05eb
Matthew Dickson <mdickson972@gmail.com>
2022-08-25 00:56:15 +0100

    Condensed data service

--- UrlShortener/Services/DataService.cs
+++ UrlShortener/Services/DataService.cs
@@ -6,56 +6,39 @@ namespace UrlShortener.Services
 {
     public class DataService : IDataService
     {
-        private readonly string ROOT_DATA_DIRECTORY = Path.Combine(Environment.CurrentDirectory, @"Data\");
-        private const string URL_MAP_FILE_NAME = "urlMap.json";
+        private string filePath;
 
         public DataService()
         {
-            // Create data directory if doesnt exist
-            CreateDirectory(ROOT_DATA_DIRECTORY);
+            var rootDataDirectory = Path.Combine(Environment.CurrentDirectory, @"Data\");
+            var urlMapFileName = "urlMap.json";
+
+            filePath = $"{rootDataDirectory}/{urlMapFileName}";
         }
 
         public void Add<T>(T content)
         {
             var contentString = JsonConvert.SerializeObject(content);
-            WriteFile(ROOT_DATA_DIRECTORY, contentString);
+            WriteFile(contentString);
         }
 
         public T Get<T>()
         {
-            var contentString = ReadFile(ROOT_DATA_DIRECTORY);
+            var contentString = ReadFile();
             return JsonConvert.DeserializeObject<T>(contentString);
-        }
-
-        /// <summary>
-        /// Creates a new directory for passed path
-        /// </summary>
-        /// <param name="path">Path to the new directory</param>
-        private void CreateDirectory(string path)
-        {
-            if (!File.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
         }
 
         /// <summary>
         /// Creates a new file for passed path
         /// </summary>
         /// <param name="path">Path to the new file</param>
-        private void WriteFile(string path, string content)
+        private void WriteFile(string content)
         {
-            var filePath = $"{path}/{URL_MAP_FILE_NAME}";
-
-            // Serialize content as json and write to file                
             File.WriteAllText(filePath, content);
         }
 
-        private string ReadFile(string path)
+        private string ReadFile()
         {
-            var filePath = $"{path}/{URL_MAP_FILE_NAME}";
-
-                           
             return File.ReadAllText(filePath);
         }
     }
